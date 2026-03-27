72bc83428ccc08ddcb22eddbb82d85f15652653b
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-03-27 15:55:44 +0100

    afficher les projets dynamiquement avec fetch

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,28 @@
 fetch("http://localhost:5678/api/works")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data); 
+    afficherTravaux(data);
+  });
+  function afficherTravaux(works) {
+  const gallery = document.querySelector(".gallery");
+
+  works.forEach(work => {
+    // créer les éléments
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+
+    // remplir les données
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figcaption.textContent = work.title;
+
+    // assembler
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+
+    // ajouter à la galerie
+    gallery.appendChild(figure);
+  });
+}

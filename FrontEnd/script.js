d8235bf9337ae5b9c9b9ce8d46965d955c6ee08b
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-03-28 22:12:13 +0100

    fetch des catégories et preparation des boutons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -26,3 +26,10 @@ fetch("http://localhost:5678/api/works")
     gallery.appendChild(figure);
   });
 }
+fetch("http://localhost:5678/api/categories")
+.then(response => response.json())
+  .then(data => {
+    console.log(data); 
+    afficherCategories(data);
+  });
+  const button = document.createElement("button");
